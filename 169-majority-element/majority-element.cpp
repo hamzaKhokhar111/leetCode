5beaec7dac6b830023c600f0e841fdde9bcab93e
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-05-22 10:18:41 +0500

    Time: 0 ms (100.00%) | Memory: 28.3 MB (6.21%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,17 +4,16 @@ using namespace std;
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> freq;
+        unordered_map<int, int> mymap;
         int n = nums.size();
 
-        for (int i = 0; i < n; i++) {
-            freq[nums[i]]++;
+       for(int i=0; i<n; i++){
+           mymap[nums[i]]++;
 
-            if (freq[nums[i]] > n / 2) {
-                return nums[i];
-            }
-        }
-
+           if(mymap[nums[i]]>n/2){
+            return nums[i];
+           }
+       }
         // Yeh line kabhi nahi chalegi kyunki question keh raha hai ke majority element hamesha hoga
         return -1;
     }
