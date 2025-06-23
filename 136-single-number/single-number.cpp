3c453964d17a872478809b37c421c00703b84ff7
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-06-23 08:50:56 +0500

    Time: 9 ms (17.59%) | Memory: 23.9 MB (9.60%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,14 +1,23 @@
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-    sort(nums.begin(),nums.end());
+        unordered_map<int, int> mymap;
 
-    for( int i=0; i<nums.size()-1;i+=2){
-        if(nums[i]!=nums[i+1]){
-            return nums[i];
+        for (int i = 0; i < nums.size(); i++) {
+            mymap[nums[i]]++;  // har number ka count barhao
         }
-    }
-    return nums[nums.size()-1];
-    }
 
+        // ab check karo konsa number sirf 1 baar aya
+        for (int i = 0; i < nums.size(); i++) {
+            if (mymap[nums[i]] == 1) {
+                return nums[i];
+            }
+        }
+
+        return -1;  // fallback
+    }
 };
