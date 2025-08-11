514cf81c55b03021029639829b99251dbca844a6
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-08-11 07:39:45 +0500

    Time: 27 ms (80.93%) | Memory: 70.9 MB (89.00%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());  // Sort the array first
-
-        for (int i = 0; i < nums.size() - 1; i++) {  // Loop till second last
-            if (nums[i] == nums[i + 1]) {  // Check adjacent elements
-                return true;  // Duplicate found
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size()-1; i++){
+            if(nums[i]==nums[i+1]){
+                return true;
             }
         }
-
-        return false;  // No duplicates
+        return false;
     }
 };
