d5d6e203f2cb6529d19e4dfb3a5a0e9f8ffbe032
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-08-17 10:56:22 +0500

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.32%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,23 +1,11 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;                 // start index
-        int right = nums.size() - 1;  // end index
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2; // middle index
-
-            if (nums[mid] == target) {
-                return mid; // target mil gaya
-            }
-            else if (nums[mid] < target) {
-                left = mid + 1; // target right side me hai
-            }
-            else {
-                right = mid - 1; // target left side me hai
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==target){
+                return i;
             }
         }
-
-        return -1; // target nahi mila
+        return -1;
     }
 };
