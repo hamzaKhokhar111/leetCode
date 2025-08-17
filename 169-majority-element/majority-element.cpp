c80923c7de0a02ba89f6cd2bd08f06615fb819fd
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-08-17 11:10:40 +0500

    Time: 1 ms (37.30%) | Memory: 28.1 MB (86.87%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> mymap;
-
-        // Count frequency
-        for (int i = 0; i < nums.size(); i++) {
+        
+        unordered_map<int , int >mymap;
+        for(int i=0; i<nums.size(); i++){
             mymap[nums[i]]++;
         }
-
-        // Find majority element
-        for (auto it : mymap) {
-            if (it.second > nums.size() / 2) {
+        
+        for(auto &it : mymap){
+            if(it.second > nums.size()/2){
                 return it.first;
             }
         }
-
-        return -1; // In case no majority element exists
+        return -1; // agar majority element na ho (lekin problem ke mutabiq hamesha hoga)
     }
 };
