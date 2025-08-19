664e2a15178c16001c9da5c8d766f376ecdee26a
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-08-19 17:23:58 +0500

    Time: 0 ms (100.00%) | Memory: 17.4 MB (99.33%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -5,39 +5,34 @@ public:
         int last = findLast(nums, target);
         return {first, last};
     }
-
-private:
+    
     int findFirst(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1, ans = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        int left = 0, right = nums.size() - 1, ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                ans = mid;        // possible answer
-                high = mid - 1;   // search left side
-            }
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
+                ans = mid;
+                right = mid - 1; // left side me aur check karo
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return ans;
     }
-
+    
     int findLast(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1, ans = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        int left = 0, right = nums.size() - 1, ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                ans = mid;        // possible answer
-                low = mid + 1;    // search right side
-            }
-            else if (nums[mid] < target) {
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
+                ans = mid;
+                left = mid + 1; // right side me aur check karo
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return ans;
