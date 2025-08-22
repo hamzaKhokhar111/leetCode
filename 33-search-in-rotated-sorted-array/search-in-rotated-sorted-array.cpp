601d21732d42485ac58f9a3e19def6c078717775
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-08-22 11:00:46 +0500

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.14%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    int search(vector<int>& A, int tar) {
-        int st = 0, end = A.size() - 1;
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
 
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            if (A[mid] == tar) return mid;
+            if (nums[mid] == target) return mid;
 
-            if (A[st] <= A[mid]) {
-                if (A[st] <= tar && tar <= A[mid]) {
-                    end = mid - 1;
+            // Check if left half is sorted
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1; // target left half me hai
                 } else {
-                    st = mid + 1;
+                    left = mid + 1; // target right half me hai
                 }
-            } else {
-                if (A[mid] <= tar && tar <= A[end]) {
-                    st = mid + 1;
+            }
+            // Else right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1; // target right half me hai
                 } else {
-                    end = mid - 1;
+                    right = mid - 1; // target left half me hai
                 }
             }
         }
-
         return -1;
     }
 };
