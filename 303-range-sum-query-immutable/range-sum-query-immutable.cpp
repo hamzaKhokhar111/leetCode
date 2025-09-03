d175d8e0584dfbd06b66592655f9c454e8fb16a9
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-09-03 11:21:45 +0500

    Time: 0 ms (100.00%) | Memory: 24 MB (23.64%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -1,24 +1,16 @@
 class NumArray {
-private:
-    vector<int> prefix;  // store prefix sums
-
+    vector<int> prefix;
 public:
-    // Constructor: yahan pe prefix sum array banana hai
     NumArray(vector<int>& nums) {
         int n = nums.size();
-        prefix.resize(n);
+        prefix.resize(n + 1, 0);
 
-        prefix[0] = nums[0];  // pehla element same hoga
-
-        // prefix[i] = nums[0] + nums[1] + ... + nums[i]
-        for (int i = 1; i < n; ++i) {
-            prefix[i] = prefix[i - 1] + nums[i];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
         }
     }
 
-    // Sum from left to right
     int sumRange(int left, int right) {
-        if (left == 0) return prefix[right];  // direct answer
-        return prefix[right] - prefix[left - 1];  // subtract previous part
+        return prefix[right + 1] - prefix[left];
     }
 };
