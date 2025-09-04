76f8f6495b17b2784bd7d50df4f85d8eb7fd7dd6
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-09-04 09:07:10 +0500

    Time: 57 ms (31.01%) | Memory: 93 MB (15.87%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<nums.size()-1; i++){
-            if(nums[i]==nums[i+1]){
+        unordered_map<int, int> mymap;
+
+        // frequency count
+        for (int i = 0; i < nums.size(); i++) {
+            mymap[nums[i]]++;
+        }
+
+        // check frequency
+        for (auto pair : mymap) {
+            if (pair.second > 1) {
                 return true;
             }
         }
+
         return false;
     }
 };
