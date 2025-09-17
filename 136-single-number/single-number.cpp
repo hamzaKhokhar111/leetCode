6be78b15327f98bfcc390d452d15992b2afb7909
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-09-17 14:53:26 +0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (57.96%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> mymap;
-
-        // frequency count
+        int result = 0;
         for (int num : nums) {
-            mymap[num]++;
+            result ^= num;   // XOR all numbers
         }
-
-        // find element with frequency 1
-        for (auto pair : mymap) {
-            if (pair.second == 1) {
-                return pair.first;
-            }
-        }
-
-        return -1; // agar nahi mila (problem statement ke hisaab se ye case nahi hoga)
+        return result;
     }
 };
