059e368181bd44c305d846b25c09f853e7873e19
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-09-19 22:28:26 +0500

    Time: 0 ms (100.00%) | Memory: 22.7 MB (50.43%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.empty()) return 0;
-
-        int j = 0; // unique element ka pointer
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] != nums[j]) {
-                j++;
-                nums[j] = nums[i];
+        int i = 0;
+        for (int j = 1; j < nums.size(); j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
             }
         }
-
-        return j + 1; // unique elements ki count
+        return i + 1;
     }
 };
