6139a49e47b990c1b7251bd5ffcb084e20f5b341
Hamza Ashraf <155771886+hamzaKhokhar111@users.noreply.github.com>
2025-12-04 11:59:01 +0500

    Time: 35 ms (37.09%) | Memory: 14.1 MB (79.86%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,7 +5,7 @@ public:
         
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
-                
+                //hmza
                 
                 if(nums[i] + nums[j] == target) {
                     return {i, j};  // Return indexes
